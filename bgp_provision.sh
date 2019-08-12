031523158bf1af04be6684587cce6796d838ad33
rajesh-16 <48307414+rajesh-16@users.noreply.github.com>
2019-08-12 12:15:05 +0530

    Update bgp_provision.sh

--- bgp_provision.sh
+++ bgp_provision.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/rajesh-16/project_silverkey
+mkdir host_vars;
+mv project_silverkey/spine*.yml host_vars;
+mv project_silverkey/leaf*.yml host_vars;
+mv project_silverkey/bgp*.yml .
 ansible-playbook bgp_provision_spine01.yml --extra-vars 'ansible_ssh_pass=CumulusLinux!'
 ansible-playbook bgp_provision_spine02.yml --extra-vars 'ansible_ssh_pass=CumulusLinux!'
 ansible-playbook bgp_provision_leaf01.yml --extra-vars 'ansible_ssh_pass=CumulusLinux!'
